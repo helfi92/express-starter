ccfb76ec7f072c9791dd82a830a3f66d6b3a847e
Ducky Hong <pacifings@gmail.com>
2017-03-13 01:55:12 +0900

    Change 'res.write' to 'res.send' to end a response (#5)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.write('<div style="padding: 20px"><h1>Neutrino</h1><p>Welcome to Express</p></div>');
+  res.send('<div style="padding: 20px"><h1>Neutrino</h1><p>Welcome to Express</p></div>');
 })
 .listen(port, () => console.log(`Running on :${port}`));
